0646f89869f274995fc97a3046f532df3e9cf003
gabriel lopes <contatogabriieldec@gmail.com>
2024-04-15 13:05:24 -0300

    adicionado a função para atualizar o valor totatl do carrinho

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const overlay = document.getElementById("overlay");
   const itensCarrinho = document.getElementById("itens-carrinho");
   const botaoAdicionarItens = document.querySelectorAll(".add-product-cart");
+  let totalCarrinho = 0;
 
   botaoAdicionarItens.forEach((btn) => {
     btn.addEventListener("click", () => {
@@ -18,6 +19,8 @@ document.addEventListener("DOMContentLoaded", function () {
     carrinho.style.display = "block";
     overlay.style.display = "block";
     document.body.style.overflow = "hidden";
+
+    atualizarTotalCarrinho();
   });
 
   fecharCarrinho.addEventListener("click", () => {
@@ -36,6 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function adicionarAoCarrinho(productIndex, itemIndex) {
     const produto = produtos[productIndex].itens[itemIndex];
+    totalCarrinho += produto.preco;
 
     const produtoNovo = document.createElement("div");
     produtoNovo.classList.add("produtos-carrinho");
@@ -47,17 +51,30 @@ document.addEventListener("DOMContentLoaded", function () {
             />
             <div class="produtos-carrinho-text">
               <h2>${produto.nome}</h2>
-              <p>Itens: <span id="quantidade-itens-carrinho">1</span></p>
+              <p>Itens: <span id="quantidade-itens-carrinho"></span></p>
             </div>
             <button class="remover"><i class="bi bi-trash"></i></button>
       `;
     itensCarrinho.appendChild(produtoNovo);
     produtoNovo
       .querySelector(".remover")
-      .addEventListener("click", () => removerProdutosDoCarrinho(produtoNovo));
+      .addEventListener("click", () =>
+        removerProdutosDoCarrinho(produtoNovo, produto.preco)
+      );
+
+    atualizarTotalCarrinho();
   }
 
-  function removerProdutosDoCarrinho(produtoNovo) {
+  function removerProdutosDoCarrinho(produtoNovo, precoProduto) {
+    totalCarrinho -= precoProduto;
+
     produtoNovo.remove();
+    atualizarTotalCarrinho();
+  }
+
+  function atualizarTotalCarrinho() {
+    const totalElement = document.getElementById("total");
+    const totalFormatado = totalCarrinho.toFixed(2).replace(".", ",");
+    totalElement.textContent = totalFormatado;
   }
 });
